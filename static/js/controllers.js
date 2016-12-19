a716b57abbe46416cd3589a098f27a44d59977a8
Ryan Breidenbach <ryan.breidenbach+github@gmail.com>
2016-12-18 18:00:47 -0600

    Remove JavaScript support

--- static/js/controllers.js
+++
@@ -1,7 +0,0 @@
-var recipeApp = angular.module('recipeApp', []);
-
-recipeApp.controller('RecipeController', ['$scope', '$http', function ($scope, $http) {
-  $http.get('/recipes/recipes.json').success(function(data) {
-    $scope.recipes = data
-  });
-}]);
