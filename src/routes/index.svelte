3657754703adc95c9cd868ecd5320f8a9cb7a8c2
Ryan Breidenbach <ryan.breidenbach@invitationhomes.com>
2022-01-03 21:55:14 -0600

    Add placeholder for loading recipes
    Add a load() function that returns dummy data for now.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load() {
+		return {
+			props: {
+				recipes: [
+                    {name:'gumbo'},
+                    {name:'jambalaya'}
+                ]
+			}
+		};
+	}
+</script>
+
+<script>
+	export let recipes;
+</script>
+
+<h1>Recipes</h1>
+<p>Here is where we will list recipes</p>
+
+<ul>
+	{#each recipes as recipe}
+		<li>{recipe.name}</li>
+	{/each}
+</ul>
